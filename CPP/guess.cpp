32e0f275b6c72ffd8ed356bb3aa8158bb4dc47f5
RamIyer1998 <ramiyer1998@gmail.com>
2018-09-02 22:18:54 -0400

    I updated the guessing game

--- CPP/guess.cpp
+++ CPP/guess.cpp
@@ -6,15 +6,27 @@ using namespace std;
 int main()
 {
     int secret, guess;
+    int count = 0;
     srand(time(NULL));
-    
+    cout << "You have 3 tries to guess the number! Good luck!" << endl;
     secret = rand() % 10 + 1;
-    do{
+    do {
+      count++;
+      //if(count == 3){
+      //  cout << "Sorry! Try again next time!" << endl;
+      //  return 0;
+      //}
       cout << "Guess the number between 1 and 10: ";
       cin >> guess;
-      if(guess != secret){
-         cout << "Sorry! Try again!";
+      if(count == 3){
+        cout << "Sorry! Try again next time!" << endl;
+        return 0;
       }
+      if(guess != secret){
+         cout << "Sorry! Try again!" << endl;
+      }
+
     }while(secret != guess);
-    cout << "Congrats!";
+    cout << "Congrats You got it in " << count << " tries!" << endl;
+    return 0;
 }
