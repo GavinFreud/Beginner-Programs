7af40423991d2bda26cd362b9c402428fdf27e2b
gavinfreud <ramiyer1998@gmail.com>
2018-09-02 20:58:28 -0400

    Messed with user input, created a square calc

---
+++ CPP/square.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cout << "Please input an integer value for me to square: ";
+    cin >> i;
+    int square = i * i;
+    cout <<"Squared value: ";
+    cout << square;
+    return 0;
+}
