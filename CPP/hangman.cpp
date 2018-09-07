5484e89916f3e1d9f0b71b97c3ff054bd046a87d
RamIyer1998 <ramiyer1998@gmail.com>
2018-09-07 18:32:11 -0400

    Changed some logic so it made more sense

--- CPP/hangman.cpp
+++ CPP/hangman.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main()
 {
-    int count = 0;
+    int count = 5;
     char charGuess;
     int looper = 0;
     string guess = "Luck is a farce";
     string hangman(guess.size(), ' ');
-    cout << guess.size() << endl;
-    cout << hangman.size() << endl;
-    cout << "You can get 5 incorrect char guesses to get the string! Good luck!" << endl;
+    //cout << guess.size() << endl;
+    //cout << hangman.size() << endl;
+    //cout << "You can get 5 incorrect char guesses to get the string! Good luck!" << endl;
     while(hangman != guess)
     {
+      cout << "You have " << count << "tries left!" << endl;
       looper = 0;
       cout << "Enter your letter: ";
       cin >> charGuess;
@@ -23,22 +24,20 @@ int main()
         {
           
           hangman.replace(i, 1, 1, charGuess);
-          cout << hangman << endl;
           looper++;
         } 
       }
-      cout << looper << endl;
-      cout << hangman << endl;
       if(looper == 0)
       {
-        count++;
+        count--;
         cout << "Nope! None of that here!" << endl;
       }
-      if(count == 5)
+      if(count == 0)
       {
         cout << hangman << " Sorry! Try again next time!" << endl;
         return 0;
       }
+      cout << hangman << endl;
     }
     cout << "Congrats!" << endl;
     return 0;
